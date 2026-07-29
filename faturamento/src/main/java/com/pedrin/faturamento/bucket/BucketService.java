5b09213068edfde7637e61a0026ec06030e97e55
PedrinL27 <pedrolangelineto0@gmail.com>
2026-07-28 22:35:55 -0300

    Added: Upload e getUrl do bucket controller

--- faturamento/src/main/java/com/pedrin/faturamento/bucket/BucketService.java
+++ faturamento/src/main/java/com/pedrin/faturamento/bucket/BucketService.java
@@ -1,9 +1,14 @@
 package com.pedrin.faturamento.bucket;
 
 import com.pedrin.faturamento.config.props.MinioProps;
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.Http;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +18,34 @@ public class BucketService {
     private final MinioProps props;
 
     public void upload(BucketFile file) {
+        try {
+            var object = PutObjectArgs
+                    .builder()
+                    .bucket(props.getBucketName())
+                    .object(file.name())
+                    .stream(file.is(), file.size(), -1L)
+                    .contentType(file.type().toString())
+                    .build();
 
+            client.putObject(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getUrl(String filename) {
-        
+        try {
+            var object = GetPresignedObjectUrlArgs
+                    .builder()
+                    .method(Http.Method.GET)
+                    .bucket(props.getBucketName())
+                    .object(filename)
+                    .expiry(1, TimeUnit.DAYS)
+                    .build();
+
+            return client.getPresignedObjectUrl(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
