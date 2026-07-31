7898eaefda5d93a426ac40f4b83bcf9ace1bbc3a
PedrinL27 <pedrolangelineto0@gmail.com>
2026-07-31 16:00:59 -0300

    Added: No DTO colocado os campos de nota fiscal e codigo rastreio

--- pedidos/src/main/java/com/pedrin/pedidos/service/AtualizacaoStatusPedidoService.java
+++ pedidos/src/main/java/com/pedrin/pedidos/service/AtualizacaoStatusPedidoService.java
@@ -5,6 +5,7 @@ import com.pedrin.pedidos.repository.PedidoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -13,9 +14,17 @@ public class AtualizacaoStatusPedidoService {
 
     private final PedidoRepository repository;
 
+    @Transactional
     public void atualizarStatus(
             Long codigo, StatusPedido status, String urlNotaFiscal, String codigoRastreio
     ) {
         log.info("Recebido o pedido n-{} para atualizar o status", codigo);
+
+        repository.findById(codigo).ifPresent(pedido -> {
+            pedido.setStatus(status);
+            pedido.setUrlNotaFiscal(urlNotaFiscal);
+            pedido.setCodigoRastreio(codigoRastreio);
+        });
+
     }
 }
